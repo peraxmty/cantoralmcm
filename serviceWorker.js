83a8974131358a150939cb02244c59d2f1a33d8b
Gerardo Perales <ing.gerardo.perales@gmail.com>
2025-08-08 09:12:37 -0600

    Agregar caching en serviceWorker

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,3 +1,10 @@
 self.addEventListener("fetch", function(event) {
     console.log(`start server worker`)
   });
+const CACHE_NAME = 'cantoral-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/assets/cantos.json',
+    // Otros archivos...
+];
